2445fe028d16f8052d9844391907e509886a3ff2
= <=>
2020-06-21 10:39:25 -0300

    test: corrected recipe puppy consumer test suit

--- src/test/unit/models/RecipePuppyConsumer.spec.ts
+++ src/test/unit/models/RecipePuppyConsumer.spec.ts
@@ -10,41 +10,39 @@ import rawRecipes from "../../fixtures/rawRecipes.json";
 import nock from "nock";
 
 describe("RecipePuppyConsumer", () => {
-  describe(".recipes", () => {
-    describe("when success", () => {
-      nock(RECIPE_PUPPY_API_URL)
-        .get("/?&i=garlic,onions")
-        .reply(STATUS_OK, rawRecipes);
+  describe("when success", () => {
+    nock(RECIPE_PUPPY_API_URL)
+      .get("/?&i=garlic,onions")
+      .reply(STATUS_OK, rawRecipes);
 
-      test("returns a list of recipes", async () => {
-        const ingredients = ["garlic", "onions"];
-        const url = "http://www.recipepuppy.com/api/?";
-        const recipesConsumer = new RecipePuppyConsumer(consumeApi, url);
+    test("returns a list of recipes", async () => {
+      const ingredients = ["garlic", "onions"];
+      const url = "http://www.recipepuppy.com/api/?";
+      const recipesConsumer = new RecipePuppyConsumer(consumeApi, url);
 
-        const recipes = await recipesConsumer.recipes(ingredients);
+      const recipes = await recipesConsumer.recipes(ingredients);
 
-        expect(recipes).toBeInstanceOf(Array);
-        expect(recipes.length).toEqual(10);
-        expect(recipes).toMatchObject(expectedRecipes);
-      });
+      expect(recipes).toBeInstanceOf(Array);
+      expect(recipes.length).toEqual(10);
+      expect(recipes).toMatchObject(expectedRecipes);
     });
+  });
 
-    describe("when fail", () => {
-      nock(RECIPE_PUPPY_API_URL)
-        .get("/?&i=garlic,onions")
-        .reply(STATUS_SERVER_ERROR, []);
+  describe("when fail", () => {
+    nock(RECIPE_PUPPY_API_URL)
+      .get("/?&i=garlic,onions")
+      .reply(STATUS_SERVER_ERROR, []);
 
-      test("returns a list of recipes", async () => {
-        const ingredients = ["garlic", "onions"];
-        const url = "http://www.recipepuppy.com/api/?";
-        const recipesConsumer = new RecipePuppyConsumer(consumeApi, url);
+    test("returns a list of recipes", async () => {
+      const ingredients = ["garlic", "onions"];
+      const url = "http://www.recipepuppy.com/api/?";
+      const recipesConsumer = new RecipePuppyConsumer(consumeApi, url);
 
-        const recipes = await recipesConsumer.recipes(ingredients);
+      const recipes = await recipesConsumer.recipes(ingredients);
 
-        expect(recipes).toBeInstanceOf(Array);
-        expect(recipes.length).toEqual(0);
-        expect(recipes).toMatchObject([]);
-      });
+      expect(recipes).toBeInstanceOf(Array);
+      expect(recipes.length).toEqual(0);
+      expect(recipes).toMatchObject([]);
     });
   });
 });
