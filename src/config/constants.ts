f0bf7efe3e01eb8894bb1fbd7172228d65769516
= <=>
2020-06-21 22:39:30 -0300

    fix: corrected env files management and updated .env.example file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,16 @@
-import { config } from 'dotenv'
+import { config } from "dotenv-flow";
 
-config()
+config();
 
-export const PORT = process.env.PORT
-export const HOST = process.env.HOST
+export const PORT = process.env.PORT;
+export const HOST = process.env.HOST;
+
+export const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+
+// ---------- HTTP Status Code
+export const STATUS_OK = 200;
+export const STATUS_SERVER_ERROR = 500;
+
+// ---------- APIs URLs
+export const RECIPE_PUPPY_API_URL = "http://www.recipepuppy.com/api/";
+export const GIPHY_API_URL = "https://api.giphy.com/v1/gifs";
