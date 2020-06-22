55591257395e7bead89a61a0b6b547597783df45
= <=>
2020-06-22 16:48:47 -0300

    feat: added timeout to requests to external services

--- src/services/consumeApi.ts
+++ src/services/consumeApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function consumeApi(url: string, queryFields: object) {
-  return axios.get(url, { params: { ...queryFields } });
+  return axios.get(url, { params: { ...queryFields }, timeout: 400 });
 }
 
 export default consumeApi;
